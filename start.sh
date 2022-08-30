981feb3d76ae83a3063e8df8d745518b935f0a13
danxue.ck <danxue.ck@antgroup.com>
2022-08-30 16:49:47 +0800

    add oci8.pc

--- start.sh
+++ start.sh
@@ -5,19 +5,9 @@ OBAGENT_PASSWORD=$2
 OB_ORACLE_USERNAME=$3
 OB_ORACLE_PASSWORD=$4
 OB_ORACLE_SERVICE_NAME=$5
-OB_SQL_PORT=$5
+OB_SQL_PORT=$6
 
 OBAGENT_DIR=/home/admin/obagent
-
-replace_property_basic
-replace_property_pipeline
-replace_module_basic
-
-export PKG_CONFIG_PATH=/home/admin/obagent
-export LD_LIBRARY_PATH=/u01/obclient/lib/:$LD_LIBRARY_PATH
-export GO111MODULE=off
-export NLS_LANG=AMERICAN_AMERICA.UTF8
-cd /home/admin/obagent && ./bin/monagent -c conf/monagent.yaml
 
 # replace field in config_properties/monagent_basic_auth.yaml
 function replace_property_basic() {
@@ -52,3 +42,13 @@ function replace_module_basic() {
     sed -i "s/{disable_http_basic_auth}/true/g" "${OBAGENT_DIR}/conf/module_config/monagent_basic_auth.yaml"
     sed -i "s/{disable_pprof_basic_auth}/true/g" "${OBAGENT_DIR}/conf/module_config/monagent_basic_auth.yaml"
 }
+
+replace_property_basic
+replace_property_pipeline
+replace_module_basic
+
+export PKG_CONFIG_PATH=/home/admin/obagent
+export LD_LIBRARY_PATH=/u01/obclient/lib/:$LD_LIBRARY_PATH
+export GO111MODULE=off
+export NLS_LANG=AMERICAN_AMERICA.UTF8
+cd /home/admin/obagent && ./bin/monagent -c conf/monagent.yaml
